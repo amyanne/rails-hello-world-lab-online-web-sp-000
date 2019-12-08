b2d754a791ed589304d5870e7cbc508b1b84650f
Amy Westcott <amy.westcott@yahoo.com>
2019-12-08 04:34:19 +0000

    Automatically backed up by Learn

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -1,4 +1,4 @@
-class HelloWorldController < ApplicationController::Base
+class HelloWorldController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
